f0b8bd25fd52380d28c21b549691c62f590618c5
Naren Dasan <naren@narendasan.com>
2017-12-20 17:33:14 -0600

    sudo

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ pip3 install -r backend/requirements.txt
 yarn --cwd frontend
 cd frontend
 trap 'kill %1; kill %2;' SIGINT
-python3 ../backend/app.py  | tee ../log/backend.log | sed -e 's/^/[backend] /' \
+sudo python3 ../backend/app.py  | tee ../log/backend.log | sed -e 's/^/[backend] /' \
 & yarn --cwd frontend start | tee ../log/frontend.log | sed -e 's/^/[frontend] /' \
