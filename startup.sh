01597b6e8a346bf7bfe09cd73fd817f17d405d5d
ACM@UIUC <acm@illinois.edu>
2018-02-14 13:07:43 -0600

    startup for unified app

--- startup.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p log 
-pip3 install -r backend/requirements.txt
-yarn --cwd frontend
-cd frontend
-trap 'kill %1; kill %2;' SIGINT
-sudo python3 ../backend/app.py  | tee ../log/backend.log | sed -e 's/^/[backend] /' \
-& yarn --cwd frontend start | tee ../log/frontend.log | sed -e 's/^/[frontend] /' \
